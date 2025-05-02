05299ee954f9d8d239ff0caecffec48fcca878f6
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-05-02 15:57:51 +0300

    stepik 9.6 + sqlite 2

---
+++ sqlite_practic.py
@@ -0,0 +1,43 @@
+# # __________________ lesson 2 _____________________
+# import sqlite3 as sq
+#
+# with sq.connect('lesson.db') as con_bd:
+#     cur = con_bd.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS players (
+#         name TEXT,
+#         sex INTEGER,
+#         old INTEGER,
+#         score INTEGER
+#     )
+#     """)
+
+# import sqlite3 as sq
+#
+# with sq.connect('lesson.db') as con_db:
+#     cur = con_db.cursor()
+#     cur.execute("DROP TABLE players")
+
+# import sqlite3 as sq
+#
+# with sq.connect('lesson.db') as con_bd:
+#     cur = con_bd.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS players (
+#         name TEXT NOT NULL,
+#         sex INTEGER NOT NULL DEFAULT 1,
+#         old INTEGER,
+#         score INTEGER
+#     )
+#     """)
+
+# import sqlite3 as sq
+#
+# with sq.connect('lesson.db') as con_db:
+#     cur = con_db.cursor()
+#     cur.execute("DROP TABLE IF EXISTS players")
+#     cur.execute("""CREATE TABLE IF NOT EXISTS players (
+#         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL,
+#         sex INTEGER NOT NULL DEFAULT 1,
+#         old INTEGER,
+#         score INTEGER
+#     )""")
